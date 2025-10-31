b1a8870f2949843c85344a1e182b4b4e40dc26b9
NikoGutk <mykolagutnyk0799@gmail.com>
2025-10-31 17:23:53 +0100

    made function that plays the round and choose the winner

--- script.js
+++ script.js
@@ -2,50 +2,52 @@ const rock = 0;
 const paper = 1;
 const scissors = 2;
 
-function getComputerChoice () {
-    randomValue = Math.floor(Math.random() * 3);
-    if (randomValue === 0) {
-        randomValue = "Rock";
-        console.log(randomValue);
-        return randomValue;
-    } else if (randomValue === 1) {
-        randomValue = "Paper";
-        console.log(randomValue);
-        return randomValue;
-    } else (randomValue === 2)
-        randomValue = "Scissors";
-        console.log(randomValue);
-        return randomValue;
-    }
+function getComputerChoice() {
+  const randomValue = Math.floor(Math.random() * 3);
 
-function getHumanChoice () {
-    const userInput = parseInt(prompt("Choose: 0 for Rock, 1 for Paper, 2 for Scissors"));
-     if (userInput === rock) {
-    console.log("You chose Rock");
-    return "Rock";
-  } else if (userInput === paper) {
-    console.log("You chose Paper");
-    return "Paper";
-  } else if (userInput === scissors) {
-    console.log("You chose Scissors");
-    return "Scissors";
+  if (randomValue === 0) {
+    console.log("Computer chose: Rock");
+    return rock;
+  } else if (randomValue === 1) {
+    console.log("Computer chose: Paper");
+    return paper;
   } else {
-    console.log("Invalid choice!");
-    return null;
+    console.log("Computer chose: Scissors");
+    return scissors;
   }
 }
 
-function playRound (getComputerChoice, getHumanChoice) {
-    if (getComputerChoice === getHumanChoice) {
-        console.log("Draw");
-        return "Draw";
-    } else if (getComputerChoice === 0 && getHumanChoice === 1) {
-        console.log("Computer wins");
-        return "Computer wins";
-    }
-}
-    
+function getHumanChoice () {
+    const userInput = parseInt(prompt("Choose: 0 for Rock, 1 for Paper, 2 for Scissors"));
 
-getComputerChoice();
-getHumanChoice();
-playRound();
+     if (userInput === rock) {
+        console.log("You chose Rock");
+        return rock;
+    } else if (userInput === paper) {
+        console.log("You chose Paper");
+        return paper;
+    } else if (userInput === scissors) {
+        console.log("You chose Scissors");
+        return scissors;
+    } else {
+         console.log("Invalid choice!");
+         return null;
+  }
+}
+
+function playRound (computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        console.log("Draw")
+    } else if ((computerChoice === rock && humanChoice === scissors) ||
+              (computerChoice === paper && humanChoice === rock) ||
+              (computerChoice === scissors && humanChoice === paper))    
+              {
+                console.log("Computer wins");
+              } else {
+                console.log("Human wins!");
+              }
+    }
+    
+const computer = getComputerChoice();
+const human = getHumanChoice();
+playRound(computer, human);
